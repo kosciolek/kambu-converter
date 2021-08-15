b4ae6f14461710ab30a07f45e875c702dae77f23
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 11:24:35 +0200

    add store to Config

--- src/configs/Config.tsx
+++ src/configs/Config.tsx
@@ -1,6 +1,13 @@
 import { ReactNode } from "react";
+import { Provider } from "react-redux";
+import { configureAppStore } from "../store";
 import { AppThemeProvider } from "../theme";
 
-export const Config = ({ children }: { children?: ReactNode }) => (
-  <AppThemeProvider>{children}</AppThemeProvider>
-);
+export const Config = ({ children }: { children?: ReactNode }) => {
+  const store = configureAppStore();
+  return (
+    <AppThemeProvider>
+      <Provider store={store}>{children}</Provider>
+    </AppThemeProvider>
+  );
+};
