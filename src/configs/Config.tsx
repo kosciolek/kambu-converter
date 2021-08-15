21f3aa89baeb3658d376a11713df112a6d558320
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 16:19:38 +0200

    persist store

--- src/configs/Config.tsx
+++ src/configs/Config.tsx
@@ -1,22 +1,43 @@
 import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 import { configureAppStore } from "../store";
 import { GlobalStyles } from "../style/GlobalStyles";
 import { AppThemeProvider } from "../theme";
 
-export const Config = ({ children }: { children?: ReactNode }) => {
-  const store = configureAppStore();
-  const queryClient = new QueryClient();
+export type ConfigProps = {
+  persist?: boolean;
+  children?: ReactNode;
+};
 
-  return (
-    <AppThemeProvider>
-      <Provider store={store}>
-        <GlobalStyles />
-        <QueryClientProvider client={queryClient}>
-          {children}
-        </QueryClientProvider>
-      </Provider>
-    </AppThemeProvider>
+export const Config = ({ children, persist = false }: ConfigProps) => {
+  /* todo refactor to pipe-like */
+  let node = children;
+
+  /* react-query */
+  const queryClient = new QueryClient();
+  node = <QueryClientProvider client={queryClient}>{node}</QueryClientProvider>;
+
+  /* Global styles */
+  node = (
+    <>
+      <GlobalStyles />
+      {node}
+    </>
   );
+
+  /* Redux */
+  const { store, persistor } = configureAppStore({
+    persist,
+  });
+  node = <Provider store={store}>{node}</Provider>;
+
+  /* Redux-persist */
+  node = <PersistGate persistor={persistor}>{node}</PersistGate>;
+
+  /* Theme */
+  node = <AppThemeProvider>{node}</AppThemeProvider>;
+
+  return <>{node}</>;
 };
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,4 +1,15 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // todo remove cyclic dep
 // eslint-disable-next-line import/no-cycle
 import { exchangeSlice } from "./slices/exchange";
@@ -9,8 +20,34 @@ export const rootReducer = combineReducers({
 
 export type RootState = ReturnType<typeof rootReducer>;
 
-export const configureAppStore = (initialState?: RootState) =>
-  configureStore({
-    reducer: rootReducer,
+export const configureAppStore = ({
+  initialState,
+  persist = false,
+}: {
+  initialState?: RootState;
+  persist?: boolean;
+}) => {
+  const persistConfig = {
+    key: "root",
+    version: 1,
+    storage,
+  };
+
+  const reducer = persist
+    ? persistReducer(persistConfig, rootReducer)
+    : rootReducer;
+
+  const store = configureStore({
+    reducer: reducer as typeof rootReducer,
     preloadedState: initialState,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        },
+      }),
   });
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+};
