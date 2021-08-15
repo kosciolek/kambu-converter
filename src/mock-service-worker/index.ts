354cfc6eb563ac4e9542e190c46fab604de3cf70
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 11:35:35 +0200

    MSW - add route for exchangerate

--- src/mock-service-worker/index.ts
+++ src/mock-service-worker/index.ts
@@ -1,5 +1,19 @@
-import { setupWorker } from "msw";
+import { rest, setupWorker } from "msw";
+import { CurrencyRatesAnswer } from "../api/exchange/methods";
 
-export const handlers = [];
+export const handlers = [
+  rest.get("https://api.exchangerate.host/latest", (req, res, ctx) => {
+    const body: CurrencyRatesAnswer = {
+      base: "EUR",
+      date: "2021-08-14",
+      rates: {
+        EUR: 1,
+        PLN: 4.565033,
+      },
+      success: true,
+    };
+    return res(ctx.status(200), ctx.json(body));
+  }),
+];
 
 export const worker = setupWorker(...handlers);
