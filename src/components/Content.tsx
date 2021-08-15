fda0a1cc0a8079dec9c33ae79a513da6f46dfd11
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 17:18:17 +0200

    comment

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,6 +1,9 @@
 import styled from "@emotion/styled";
 import { contentWidth } from "../style/const";
 
+/**
+ * Center and constraint content width.
+ */
 export const Content = styled.div`
   margin: 0 auto;
   max-width: ${contentWidth};
