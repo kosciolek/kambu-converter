24ebe9625bf1be9efafc89b97b93c60bcd1cf607
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 10:24:09 +0200

    Add emotion theme

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { Config } from "./configs/Config";
 import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Config>
+      <App />
+    </Config>
   </React.StrictMode>,
   document.getElementById("root")
 );
