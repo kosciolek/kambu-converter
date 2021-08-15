bc699287e2ba39036325484326bd7f81387162af
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 12:41:52 +0200

    Dialog test

---
+++ src/components/Dialog/Dialog.test.tsx
@@ -0,0 +1,32 @@
+import {
+  screen,
+  render,
+  fireEvent,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import { Config } from "../../configs/Config";
+import { Dialog } from "./index";
+
+describe("Dialog", () => {
+  it("Opens and closes", async () => {
+    render(
+      <Dialog
+        action={({ open }) => (
+          <button type="button" onClick={open}>
+            Open
+          </button>
+        )}
+        content={({ close }) => (
+          <button type="button" onClick={close}>
+            Close
+          </button>
+        )}
+      />,
+      { wrapper: Config }
+    );
+
+    fireEvent.click(await screen.findByText("Open"));
+    fireEvent.click(await screen.findByText("Close"));
+    await waitForElementToBeRemoved(() => screen.queryByText("Close"));
+  });
+});
