a2564e8678df0bb9db0642a03936cb55c250f1e3
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-15 10:14:28 +0200

    add forwardCalls

--- src/utils/js.ts
+++ src/utils/js.ts
@@ -2,5 +2,5 @@ export const forwardCalls =
   <FUNC extends (...args: any[]) => void>(
     ...funcs: (FUNC | undefined | null)[]
   ) =>
-    (...params: Parameters<FUNC>) =>
-      funcs.forEach((func) => func?.(...params));
+  (...params: Parameters<FUNC>) =>
+    funcs.forEach((func) => func?.(...params));
