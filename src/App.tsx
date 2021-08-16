102b8cd7b2ce0efadd7192bb98133855016effe5
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-16 10:53:48 +0200

    Global layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import styled from "@emotion/styled";
 import React from "react";
+import { Controls } from "./components/Controls";
+import { Statistics } from "./components/Statistics";
+import { Topbar } from "./components/Topbar";
+import { Transactions } from "./components/Transactions";
+import { media } from "./style/media";
 
 function App() {
-  return <div />;
+  return (
+    <Layout>
+      <div style={{ gridArea: "top" }}>
+        <Topbar />
+      </div>
+      <div style={{ gridArea: "statistics" }}>
+        <Statistics />
+      </div>
+      <div style={{ gridArea: "content" }}>
+        <Controls />
+        <Transactions />
+      </div>
+    </Layout>
+  );
 }
+
+const Layout = styled.div`
+  display: grid;
+  flex-grow: 1;
+  min-height: 100vh;
+
+  grid-template:
+    "top" auto
+    "statistics" auto
+    "content" 1fr / 1fr;
+
+  ${media.md} {
+    grid-template:
+      "top top" auto
+      "statistics content" 1fr / minmax(300px, 420px) 1fr;
+  }
+`;
 
 export default App;
