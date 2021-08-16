1bbcf23b929646f877dae03cff462a9157db4c51
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-16 11:18:25 +0200

    make useId prefixable

--- src/hooks/useId.ts
+++ src/hooks/useId.ts
@@ -4,7 +4,7 @@ import { getId } from "../utils/js";
 /**
  * Generate an ID stable over the lifecycle.
  */
-export const useId = () => {
-  const [id] = useState(getId);
+export const useId = (prefix?: string) => {
+  const [id] = useState(() => `${String(prefix)}-${getId()}`);
   return id;
 };
