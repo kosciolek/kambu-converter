88466170ff4bbf624a54c0d7b81677b2c9e3772b
kosciolek <45483493+kosciolek@users.noreply.github.com>
2021-08-16 10:57:00 +0200

    Add initialState prop to Config

--- src/configs/Config.tsx
+++ src/configs/Config.tsx
@@ -2,16 +2,21 @@ import { ReactNode } from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-import { configureAppStore } from "../store";
+import { configureAppStore, RootState } from "../store";
 import { GlobalStyles } from "../style/GlobalStyles";
 import { AppThemeProvider } from "../theme";
 
 export type ConfigProps = {
   persist?: boolean;
   children?: ReactNode;
+  initialState?: RootState;
 };
 
-export const Config = ({ children, persist = false }: ConfigProps) => {
+export const Config = ({
+  children,
+  persist = false,
+  initialState,
+}: ConfigProps) => {
   /* todo refactor to pipe-like */
   let node = children;
 
@@ -29,6 +34,7 @@ export const Config = ({ children, persist = false }: ConfigProps) => {
 
   /* Redux */
   const { store, persistor } = configureAppStore({
+    initialState,
     persist,
   });
   node = <Provider store={store}>{node}</Provider>;
